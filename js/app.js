7d89418bdb44fc806e9ecb0e7bba098003078223
akki-dev <62125672+akki-dev@users.noreply.github.com>
2020-03-30 23:35:33 -0500

    update

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function suma() {
+    var n1 = document.getElementById('quantity').value;
+    var n1 = Number(n1);
+    var ma = n1 ;
+    document.getElementById('total').value = ma.toFixed(2);
+    console.log(ma.toFixed(2));
+    
+}
